08fc72b327c71fa5c63d523e44cdc366fc65b835
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2016-04-18 14:20:48 +0100

    create an input field to add custom todos

--- controllers.js
+++ controllers.js
@@ -1,4 +1,10 @@
 toDoApp.controller('ToDoController', function() {
-  this.todos = [{ text: "ToDo1", completed: true},
+
+  var self = this;
+
+  self.todos = [{ text: "ToDo1", completed: true},
                 { text: "ToDo2", completed: false}];
+  self.addToDo = function(todoText) {
+    self.todos.push({ text: todoText, completed: false});
+  };
 });
